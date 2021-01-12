25015e77f0b259634ae43744924a1601909ec64b
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-01-12 14:32:46 +0100

    commenti

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+
+//creo funzione che al click sull'icona dell'hamburger menu mostri il menu nascosto
 $('.fa-bars').click(function(){
   $('.hamburger-menu').show()
 });
 
+
+// creo funzione che al click sulla X chiuda il menu
 $('.fa-times').click(function(){
   $('.hamburger-menu').hide()
 });
